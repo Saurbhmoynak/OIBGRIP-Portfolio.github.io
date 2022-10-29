33d74ed44ea843351cbc97d32b8e839e11908a2d
Saurbh Moynak <94280322+Saurbhmoynak@users.noreply.github.com>
2022-10-29 19:40:30 +0530

    Contact Form Updated

--- javascript.js
+++ javascript.js
@@ -21,3 +21,46 @@ let scrollTop = document.querySelector('.scroll-top')
 window.addEventListener("scroll", () => {
   scrollTop.classList.toggle("scroll-active", window.scrollY > 400);
 });
+
+
+function validate() {
+  let name = document.querySelector(".name");
+  let email = document.querySelector(".email");
+  let msg = document.querySelector(".message");
+  let sendBtn = document.querySelector(".send-btn");
+
+  sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (name.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    } else {
+      sendmail(name.value, email.value, msg.value);
+      success();
+    }
+  });
+}
+validate();
+
+function sendmail(name, email, msg) {
+  emailjs.send("service_8ve0kgq","template_6gdgoid",{
+    from_name: email,
+    to_name: name,
+    message: msg,
+    });
+}
+
+function emptyerror() {
+  swal({
+    title: "Oh no.....!",
+    text: "Fields cannot be empty!",
+    icon: "error",
+  });
+}
+
+function success() {
+  swal({
+    title: "Email sent successfully",
+    text: "We try to reply in 24 hours",
+    icon: "success",
+  });
+}
